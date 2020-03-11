d40856b48d09b93e69285e64916d300a9405a47f
Rodrigo Guimarães <guimaraes_rodrigues@yahoo.com>
2020-03-11 10:07:59 -0300

    new main.py version and pipenv config

--- 01/main.py
+++ 01/main.py
@@ -1,55 +1,106 @@
-import cv2 as cv
+# ===============================================================================
+# Exemplo: segmentação de uma imagem em escala de cinza.
+# -------------------------------------------------------------------------------
+# Autor: Bogdan T. Nassu
+# Universidade Tecnológica Federal do Paraná
+# ===============================================================================
+
 import sys
+import timeit
 import numpy as np
+import cv2
 
-threshold = 0.7
+# ===============================================================================
 
+INPUT_IMAGE = 'arroz.bmp'
 
-def main():
-    sys.setrecursionlimit(650000)
-
-    img = cv.imread('arroz.bmp', cv.IMREAD_GRAYSCALE)
-
-    # oldColor = 1;
-    #
-    # newColor = oldColor ? 0: 1;
-
-    # rotula(2, img, 0, 0)
-
-    img_out = img
-
-    img_bin = binariza(img, img_out, threshold)
-
-    # for i in range(img_out.shape[0]):
-    #     for j in range(img_out.shape[1]):
-    #         if img_bin[i][j] != 0.0:
-    #             print(img_bin[i][j])
-
-    # show_img("Binarizada", img_bin)
-
-    print("Quantidade de arroz: ", rotula(img_bin, 20, 30, 60))
+# TODO: ajuste estes parâmetros!
+NEGATIVO = False
+THRESHOLD = 0.7
+ALTURA_MIN = 1
+LARGURA_MIN = 1
+N_PIXELS_MIN = 1
 
 
-def show_img(title, img):
-    cv.imshow(title, img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+# ===============================================================================
 
+def binariza(img, threshold):
+    ''' Binarização simples por limiarização.
 
-def binariza(img_in, img_out, threshold):
-    if img_in.shape != img_out.shape:
-        print("ERRO: binariza: as imagens precisam ter o mesmo tamanho e numero de canais.")
-    else:
-        img_out = cv.normalize(img_in.astype('float'), None, 0.0, 1.0, cv.NORM_MINMAX)
+Parâmetros: img: imagem de entrada. Se tiver mais que 1 canal, binariza cada
+              canal independentemente.
+            threshold: limiar.
 
-        for i in range(img_out.shape[0]):
-            for j in range(img_out.shape[1]):
-                if img_out[i][j] > threshold:
-                    img_out[i][j] = -1.0
-                else:
-                    img_out[i][j] = 0
+Valor de retorno: versão binarizada da img_in.'''
 
-        return img_out
+    # TODO: escreva o código desta função.
+    # Dica/desafio: usando a função np.where, dá para fazer a binarização muito
+    # rapidamente, e com apenas uma linha de código!
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i][j] > threshold:
+                img[i][j] = -1.0
+            else:
+                img[i][j] = 0
+
+    return img
+
+# -------------------------------------------------------------------------------
+
+def rotula(img, largura_min, altura_min, n_pixels_min):
+    '''Rotulagem usando flood fill. Marca os objetos da imagem com os valores
+[0.1,0.2,etc].
+
+Parâmetros: img: imagem de entrada E saída.
+            largura_min: descarta componentes com largura menor que esta.
+            altura_min: descarta componentes com altura menor que esta.
+            n_pixels_min: descarta componentes com menos pixels que isso.
+
+Valor de retorno: uma lista, onde cada item é um vetor associativo (dictionary)
+com os seguintes campos:
+
+'label': rótulo do componente.
+'n_pixels': número de pixels do componente.
+'T', 'L', 'B', 'R': coordenadas do retângulo envolvente de um componente conexo,
+respectivamente: topo, esquerda, baixo e direita.'''
+
+    # TODO: escreva esta função.
+    # Use a abordagem com flood fill recursivo.
+
+    label = 1
+
+    componentsList = []
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i][j] == -1.0:
+                floodfill(label, img, i, j)
+                label += 1
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i][j] >= 1:
+                new_component = {'label': img[i][j], 'x0': i, 'y0': j, 'x': i, 'y': j, 'qtd_pixels': 0}
+
+                new_component = defineComponents(img, new_component, i, j)
+
+                componentsList.append(new_component)
+
+    for component in componentsList:
+        if (component['x'] - component['x0']) < altura_min:
+            componentsList.remove(component)
+            continue
+
+        if (component['y'] - component['y0']) < largura_min:
+            componentsList.remove(component)
+            continue
+
+        if component['qtd_pixels'] < n_pixels_min:
+            componentsList.remove(component)
+            continue
+
+    return len(componentsList)
 
 
 def floodfill(label, img, x0, y0):
@@ -100,43 +151,47 @@ def defineComponents(img, component, x0, y0):
     defineComponents(img, component, x0, y0 - 1)
 
     return component
+# ===============================================================================
+
+def main():
+    # Abre a imagem em escala de cinza.
+    img = cv2.imread(INPUT_IMAGE, cv2.IMREAD_GRAYSCALE)
+    if img is None:
+        print('Erro abrindo a imagem.\n')
+        sys.exit()
+
+    # É uma boa prática manter o shape com 3 valores, independente da imagem ser
+    # colorida ou não. Também já convertemos para float32.
+    img = img.reshape((img.shape[0], img.shape[1], 1))
+    img = img.astype(np.float32) / 255
+
+    # Mantém uma cópia colorida para desenhar a saída.
+    img_out = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    # Segmenta a imagem.
+    if NEGATIVO:
+        img = 1 - img
+    img = binariza(img, THRESHOLD)
+    cv2.imshow('01 - binarizada', img)
+    cv2.imwrite('01 - binarizada.png', img * 255)
+
+    start_time = timeit.default_timer()
+    # componentes = rotula(img, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
+    # n_componentes = len(componentes)
+    print('Tempo: %f' % (timeit.default_timer() - start_time))
+    # print('%d componentes detectados.' % n_componentes)
+
+    # Mostra os objetos encontrados.
+    # for c in componentes:
+    #     cv2.rectangle(img_out, (c['L'], c['T']), (c['R'], c['B']), (0, 0, 1))
+
+    cv2.imshow('02 - out', img_out)
+    cv2.imwrite('02 - out.png', img_out * 255)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 
-def rotula(img, largura_min, altura_min, n_pixels_min):
+if __name__ == '__main__':
+    main()
 
-    label = 1
-
-    componentsList = []
-
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            if img[i][j] == -1.0:
-                floodfill(label, img, i, j)
-                label += 1
-
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            if img[i][j] >= 1:
-                new_component = {'label': img[i][j], 'x0': i, 'y0': j, 'x': i, 'y': j, 'qtd_pixels': 0}
-
-                new_component = defineComponents(img, new_component, i, j)
-
-                componentsList.append(new_component)
-
-    for component in componentsList:
-        if (component['x'] - component['x0']) < altura_min:
-            componentsList.remove(component)
-            continue
-
-        if (component['y'] - component['y0']) < largura_min:
-            componentsList.remove(component)
-            continue
-
-        if component['qtd_pixels'] < n_pixels_min:
-            componentsList.remove(component)
-            continue
-
-    return len(componentsList)
-
-
-if __name__ == '__main__': main()
+# ===============================================================================
