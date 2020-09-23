b3ac7e0bdec33edb3d838208d8a3a4a88df1fb00
Rodrigo Guimarães <guimaraes_rodrigues@yahoo.com>
2020-09-23 17:44:30 -0300

    04 fix blob avg size

--- 04/main.py
+++ 04/main.py
@@ -5,12 +5,16 @@
 # Universidade Tecnológica Federal do Paraná
 # ===============================================================================
 import sys
+from statistics import pstdev
+
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 # ===============================================================================
+from matplotlib.ticker import PercentFormatter
 
-INPUT_IMAGE = '150.bmp'
+INPUT_IMAGE = '60.bmp'
 NEGATIVO = False
 
 
@@ -21,19 +25,23 @@ def main():
     # Mantém uma cópia colorida para desenhar a saída.
     img_out = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-    # Segmenta a imagem.
-    if NEGATIVO:
-        img = 1 - img
+    # Remove ruídos e binariza
+    img_bin = pre_process(img)
 
-    img = pre_process(img)
+    # Rotula componentes
+    img = labeling(img_bin)
 
-    # show_img(img)
-
-    img = labeling(img)
-
+    # Identifica tamanho dos blobs
     blobs = identify_blobs(img)
 
-    calc_blob_size_avg(blobs)
+    # Desenha um retangulo em volta de cada blob
+    draw_rectangles(img_out, blobs)
+
+    # Conta quantos grãos de arroz existem na imagem
+    print(count_blobs(blobs))
+
+    # Exibe imagem com os blobs identificados
+    show_img(img_out)
 
 
 def pre_process(img):
@@ -41,7 +49,7 @@ def pre_process(img):
 
     img = cv2.subtract(img, img_blurred)
 
-    img = cv2.threshold(img, 0.2, 1, cv2.THRESH_BINARY)[1]
+    img = cv2.threshold(img, 0.16, 1, cv2.THRESH_BINARY)[1]
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
 
@@ -96,30 +104,30 @@ def identify_blobs(img):
 
 
 def calc_blob_size_avg(blobs_dict):
-    # blob_max_size = next(iter(blobs_dict.values()))['n_pixels']
-    # blob_min_size = next(iter(blobs_dict.values()))['n_pixels']
 
     blobs_sorted = sorted(blobs_dict.items(), key=lambda x: x[1]['n_pixels'])
+
+    blob_sorted_size = len(blobs_sorted)
     blob_sum = 0
-    for elem in blobs_sorted[5:-5]:
+
+    for elem in blobs_sorted[round(blob_sorted_size/20):-round(blob_sorted_size/3)]:
         blob_sum += elem[1]['n_pixels']
 
-    return round(blob_sum / len(blobs_sorted[5:-5]))
+    return round(blob_sum / len(blobs_sorted[round(blob_sorted_size/20):-round(blob_sorted_size/3)]))
 
 
-    # blob_sum = 0
-    # for key in blobs_dict:
-    #     blob_size = blobs_dict[key]['n_pixels']
-    #     blob_sum += blobs_dict[key]['n_pixels']
-    #
-    #     if blob_size > blob_max_size:
-    #         blob_max_size = blob_size
-    #     elif blob_size < blob_min_size:
-    #         blob_min_size = blob_size
-    #
-    # print(blob_max_size)
-    # print(blob_min_size)
-    # print(blob_sum / len(blobs_dict.keys()))
+def count_blobs(blobs):
+    blob_avg_size = calc_blob_size_avg(blobs)
+
+    n_blobs = 0
+    for key in blobs:
+        blob_quantity = blobs[key]['n_pixels'] / blob_avg_size
+        if blob_quantity < 1:
+            n_blobs += 1
+        else:
+            n_blobs += round(blob_quantity)
+
+    return n_blobs
 
 
 def open_img(file_name):
@@ -130,8 +138,12 @@ def open_img(file_name):
 
     # É uma boa prática manter o shape com 3 valores, independente da imagem ser
     # colorida ou não. Também já convertemos para float32.
-    img = img.reshape((img.shape[0], img.shape[1], 1))
+    # img = img.reshape((img.shape[0], img.shape[1], 1))
     img = img.astype(np.float32) / 255
+
+    # Segmenta a imagem.
+    if NEGATIVO:
+        img = 1 - img
 
     return img
 
@@ -140,6 +152,11 @@ def show_img(img):
     cv2.imshow('img', img)
     cv2.waitKey()
     cv2.destroyAllWindows()
+
+
+def draw_rectangles(img_out, blobs_dict):
+    for b in blobs_dict.values():
+        cv2.rectangle(img_out, (b['L'], b['T']), (b['R'], b['B']), (0, 0, 1))
 
 
 if __name__ == '__main__':
